e1d5ad446f9ef0ece0068a6256f554928975cab3
Shev <sebabenitez1095@gmail.com>
2020-01-27 23:41:30 -0300

    Add project files.

---
+++ Soluciones/Clases/Factorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soluciones.Clases
+{
+    public class Factorial
+    {
+        public int calcularFactorial(int numero)
+        {
+            int factorial = 1;
+            for (int i = numero; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        public int calcularFactorialRecursiva(int numero)
+        {
+            if (numero == 1)
+            {
+                return 1;
+            }
+            return numero * calcularFactorialRecursiva(numero - 1);
+        }
+    }
+}
---
+++ Soluciones/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Soluciones.Clases;
+
+namespace Soluciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factorial = new Factorial();
+            Console.WriteLine(factorial.calcularFactorial(5));
+            Console.WriteLine(factorial.calcularFactorialRecursiva(5));
+            Console.ReadKey();
+        }
+    }
+}
