698d476460f843ff755dfc8400399af9212996ee
Shev <sebabenitez1095@gmail.com>
2020-01-28 10:45:29 -0300

    Add project files.

---
+++ Ejercicios28012020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicios28012020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Soluciones/Clases/IngresoNumeros.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soluciones.Clases
+{
+    class IngresoNumeros
+    {
+        List<int> _numeros;
+        public IngresoNumeros()
+        {
+            _numeros = new List<int>();
+        }
+
+        private bool validarNumero(int numero)
+        {
+            if (numero < 0 || numero > 100) return false;
+            return true;
+        }
+        public void cargarNumero(int numero)
+        {
+            if (validarNumero(numero)) _numeros.Add(numero);
+        }
+        public int cantidadNumeros()
+        {
+            return _numeros.Count;
+        }
+        public int obtenerMayor()
+        {
+            return 0;
+        }
+        public int obtenerMenor()
+        {
+            return 0;
+        }
+        public int obtenerPromedio()
+        {
+            return 0;
+        }
+        public int obtenerSuma()
+        {
+            return 0;
+        }
+    }
+}
